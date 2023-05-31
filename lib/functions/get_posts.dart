b7989139bac9caf1d4e3ec1744541cb286352ba1
APIgames2 <111185287+APIgames2@users.noreply.github.com>
2023-05-31 21:34:35 +0200

    scroll infini

--- lib/functions/get_posts.dart
+++ lib/functions/get_posts.dart
@@ -1,14 +1,37 @@
-// ignore_for_file: void_checks
+// ignore_for_file: non_constant_identifier_names
+
+import 'dart:async';
 
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 
-Future <void> get_posts() async {
-  var dataapp;
-  DatabaseReference database = FirebaseDatabase.instance.ref('posts');
-  database.onValue.listen((DatabaseEvent event) {
-    final data = event.snapshot.value;
-    print(data);
-    dataapp = data;
+class TrendData {
+  final String id;
+
+  TrendData(this.id);
+}
+
+Future<String?> get_posts() async {
+  DatabaseReference trends = FirebaseDatabase.instance.ref('trends');
+  DatabaseReference posts = FirebaseDatabase.instance.ref('posts');
+
+  Completer<String?> completer = Completer<String?>();
+
+  trends.onValue.listen((DatabaseEvent event) {
+    final trendsData = event.snapshot.value as List<dynamic>?;
+    final trendObjects = trendsData
+        ?.map((data) => TrendData(data["id"].toString().trim()))
+        .toList();
+    print(trendObjects?[1].id.trim());
+
+    posts.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value as Map<dynamic, dynamic>?;
+
+      String? imageUrl = data?[trendObjects?[1].id.trim()]?["images"]?["url"];
+      print("Le premier post est $imageUrl");
+      completer.complete(imageUrl);
+    });
   });
-  return dataapp;
+
+  return completer.future;
 }
